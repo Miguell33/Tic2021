9c65aa315a997df4d045856744b82efea6b04809
Miguell33 <71437895+Miguell33@users.noreply.github.com>
2020-09-24 12:59:43 +0200

    Add files via upload

---
+++ suma_gauss.py
@@ -0,0 +1,9 @@
+﻿def suma_gaus():
+    n_final=100
+    suma_acumulada=0
+    for cont in range(1,n_final+1):
+        suma_acumulada = suma_acumulada+cont
+    print "La suma de los números hasta ", n_final
+    print " = ", suma_acumulada
+
+suma_gaus()
