7c69d76320183f9d3ec141de90a5665a6ca0112f
Miguell33 <71437895+Miguell33@users.noreply.github.com>
2020-10-22 12:51:12 +0200

    Add files via upload

---
+++ ejercicio 2.py
@@ -0,0 +1,10 @@
+'''
+Escriba una función que reciba una cantidad
+en dólares y devuelva su cambio en euros.
+'''
+
+def convertidor():
+    dolares=input("Introduce cantidad en dolares: ")
+    print dolares, "dolares, son: ", dolares*0.84, "euros."
+
+convertidor()
---
+++ ejercicio 3.py
@@ -0,0 +1,27 @@
+'''
+Escriba una función que reciba un número entero del
+1 al 7 y escriba en pantalla el correspondiente
+día de la semana.
+'''
+
+def semanita():
+    numero=input("Introduce un numero del 1 al 7: ")
+    if numero==1:
+        print "Hoy es Lunes"
+    if numero==2:
+        print "Hoy es martes"
+    if numero==3:
+        print "Hoy es miercoles"
+    if numero==4:
+        print "Hoy es jueves"
+    if numero==5:
+        print "Hos es viernes"
+    if numero==6:
+        print "Hoy es sabado"
+    if numero==7:45
+    
+        print "Hoy es domingo"
+
+semanita()
+                                
+    
