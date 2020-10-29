b13af2623a014f16fe5a78608e13776afb9453be
Miguell33 <71437895+Miguell33@users.noreply.github.com>
2020-10-29 12:53:58 +0100

    Add files via upload

--- Ejercicio6.2.py
+++ Ejercicio6.2.py
@@ -11,27 +11,36 @@ y su resultado.
 '''
 
 def menu():
+    interruptura=1
     print "Introduce dos numeros enteros:"
     numero1=input("numero1= ")
-    numero2=input("numero2= ")
-    print "***************************************"
-    print "*               MENU                  *"
-    print "***************************************"
-    print "1. SUMA"
-    print "2. RESTA"
-    print "3. MULTIPLICACION"
-    print "4. DIVISION"
-    opcion=input("Teclee la opcion elegida")
-    if(opcion==1):
-        resultado=numero1+numero2
-    if(opcion==2):
-        resultado=numero1-numero2
-    if(opcion==3):
-        resultado=numero1*numero2
-    if(opcion==4):
-        if(numero2==0):
-            print "Ande vas cabezon que eso es imposible"
-        resultado=numero1/numero2
+    while(interruptura==1):
+        numero2=input("numero2= ")
+        print "***************************************"
+        print "*               MENU                  *"
+        print "***************************************"
+        print "1. SUMA"
+        print "2. RESTA"
+        print "3. MULTIPLICACION"
+        print "4. DIVISION"
+        opcion=input("Teclee la opcion elegida")
+        if(opcion==1):
+            resultado=numero1+numero2
+            interruptura=0
+        if(opcion==2):
+            resultado=numero1-numero2
+            interruptura=0
+        if(opcion==3):
+            resultado=numero1*numero2
+            interruptura=0
+        if(opcion==4):
+            if(numero2<>0):
+                resultado=numero1/numero2
+                interruptura=0
+            else:
+                interruptura=1
+                print "Has intentado dividir entre 0"
+                print "Introduce un nuevo numero"
     print "El resultado es ", resultado
 
 menu()
