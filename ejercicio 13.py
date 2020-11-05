02143cdbe2ebce307325a3d972e2b2c62b1907e9
Miguell33 <71437895+Miguell33@users.noreply.github.com>
2020-11-05 12:38:12 +0100

    Add files via upload

---
+++ ejercicio 13.py
@@ -0,0 +1,10 @@
+'''Escriba una función que imprima por
+pantalla una pirámide como la de la figura:
+'''
+def piramide():
+    print "    *    "
+    print "   ***   "
+    print " ******* "
+    print "*********"
+
+piramide()
---
+++ ejercico 10.py
@@ -0,0 +1,11 @@
+''' Realizar un programa que al recibir un numero entero
+muestre por pantalla los 3 numeros anteriores y los
+3 numeros siguientes el numero recibido
+'''
+def ejercicio10():
+    numero= input ("Que numero deseas saber: ")
+    print (numero-1),(numero-2),(numero-3)
+    print (numero+1),(numero+2),(numero+3)
+
+ejercicio10()
+    
